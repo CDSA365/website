c2aa0442e0ff8e52e18afe0e02695b6424193fd4
samnsimson <samnishanth01@gmail.com>
2022-10-17 19:39:30 +0530

    script file added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,7 +24,33 @@ export default class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
+                    <meta
+                        name="google-site-verification"
+                        content="TQzQ2whZRQK2Gt35GAlfkEairavXg7ASktjgKEqZ_8g"
+                    />
                     <link rel="icon" href="/favicon.ico" />
+                    <script
+                        async
+                        src="https://www.googletagmanager.com/gtag/js?id=UA-131650902-1"
+                    ></script>
+                    <script>
+                        function gtag(){dataLayer.push(arguments)}
+                        window.dataLayer=window.dataLayer||[],gtag("js",new
+                        Date),gtag("config","UA-131650902-1")
+                    </script>
+                    <script
+                        async
+                        src="https://www.googletagmanager.com/gtag/js?id=G-7WQBH1E1N7"
+                    ></script>
+                    <script>
+                        function gtag(){dataLayer.push(arguments)}
+                        window.dataLayer=window.dataLayer||[],gtag("js",new
+                        Date),gtag("config","G-7WQBH1E1N7")
+                    </script>
+                    <script
+                        defer="defer"
+                        src="/static/js/main.e07f7365.js"
+                    ></script>
                 </Head>
                 <body>
                     <Main />
