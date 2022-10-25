fee8728916c40e6fea334661db09d18411b587e3
samnsimson <samnishanth01@gmail.com>
2022-10-25 15:56:50 +0530

    standard layout for 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -6,6 +6,7 @@ import Fuse from "fuse.js";
 import Link from "next/link";
 import { Chip } from "@mui/material";
 import { capitalize } from "lodash";
+import StandardLayout from "../layouts/standard";
 
 type Props = {
     pages: string[];
@@ -24,38 +25,44 @@ const NotFoundPage: NextPage = (props: Partial<Props>) => {
     }, [pages, router.asPath]);
 
     return (
-        <div className="h-[100vh] flex items-center">
-            <div className="max-w-3xl mx-auto flex flex-col items-center gap-6">
-                <img src="/images/page-not-found.png" alt="404" width="400px" />
-                <div className="text-center prose max-w-none">
-                    <h1>
-                        Page your are trying to access
-                        <br />
-                        could not be found
-                    </h1>
-                    {results?.length != 0 && (
-                        <div className="font-semibold text-red-500">
-                            Visit these pages instead?
-                        </div>
-                    )}
-                </div>
-                <div className="flex gap-3">
-                    {results?.map((result) => (
-                        <Link key={result} href={result}>
-                            <Chip
-                                color="primary"
-                                variant="outlined"
-                                label={result
-                                    .replace("/", "")
-                                    .split("-")
-                                    .map((x) => capitalize(x))
-                                    .join(" ")}
-                            />
-                        </Link>
-                    ))}
+        <StandardLayout>
+            <div className="h-[90vh] flex items-center">
+                <div className="max-w-3xl mx-auto flex flex-col items-center gap-6">
+                    <img
+                        src="/images/page-not-found.png"
+                        alt="404"
+                        width="400px"
+                    />
+                    <div className="text-center prose max-w-none">
+                        <h1>
+                            Page your are trying to access
+                            <br />
+                            could not be found
+                        </h1>
+                        {results?.length != 0 && (
+                            <div className="font-semibold text-red-500">
+                                Visit these pages instead?
+                            </div>
+                        )}
+                    </div>
+                    <div className="flex gap-3">
+                        {results?.map((result) => (
+                            <Link key={result} href={result}>
+                                <Chip
+                                    color="primary"
+                                    variant="outlined"
+                                    label={result
+                                        .replace("/", "")
+                                        .split("-")
+                                        .map((x) => capitalize(x))
+                                        .join(" ")}
+                                />
+                            </Link>
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
+        </StandardLayout>
     );
 };
 
