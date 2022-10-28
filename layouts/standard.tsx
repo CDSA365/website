47b1ff91c6be1a5b04a5898921cf45689ca4ea11
samnsimson <samnishanth01@gmail.com>
2022-10-28 22:33:09 +0530

    changes

--- layouts/standard.tsx
+++ layouts/standard.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { FC, ReactNode, useEffect } from "react";
+import { FC, ReactNode } from "react";
 import Footer from "../components/footer";
 import Header from "../components/header";
 
@@ -18,18 +18,18 @@ const StandardLayout: FC<Props> = ({
     children,
     ...props
 }) => {
-    const defaultTitle = `Online Spoken English Classes, Communication Skills Training,  Spoken English Courses Coimbatore, Best English Speaking Courses, Public Speaking Skills Coimbatore`;
-    const defaultDesc = `Carpe Diem Skills Academy offers the best online spoken English classes and communication training in Coimbatore, India. We have a team of trainers who are experienced in improving interpersonal communication skills.`;
-    const defaultKeyword = `Online Spoken English Classes,Spoken English Classes Online,Spoken English Courses Online,Online Spoken English Courses,Spoken English Courses,Spoken English Classes,Best English Speaking Courses,Best English Speaking Courses Online,Best Spoken English Classes Online,Best Spoken English Classes,Communication Skills Training in Coimbatore,Public Speaking Skills Coimbatore`;
-    const seoTitle = title?.length ? title : defaultTitle;
-    const seoDesc = description?.length ? description : defaultDesc;
-    const seoKeyword = keyword?.length ? keyword : defaultKeyword;
+    // const defaultTitle = `Online Spoken English Classes, Communication Skills Training,  Spoken English Courses Coimbatore, Best English Speaking Courses, Public Speaking Skills Coimbatore`;
+    // const defaultDesc = `Carpe Diem Skills Academy offers the best online spoken English classes and communication training in Coimbatore, India. We have a team of trainers who are experienced in improving interpersonal communication skills.`;
+    // const defaultKeyword = `Online Spoken English Classes,Spoken English Classes Online,Spoken English Courses Online,Online Spoken English Courses,Spoken English Courses,Spoken English Classes,Best English Speaking Courses,Best English Speaking Courses Online,Best Spoken English Classes Online,Best Spoken English Classes,Communication Skills Training in Coimbatore,Public Speaking Skills Coimbatore`;
+    // const seoTitle = title?.length ? title : defaultTitle;
+    // const seoDesc = description?.length ? description : defaultDesc;
+    // const seoKeyword = keyword?.length ? keyword : defaultKeyword;
     return (
         <>
             <Head>
-                <title>{seoTitle}</title>
-                <meta name="description" content={seoDesc} />
-                <meta name="keywords" content={seoKeyword} />
+                <title>{title}</title>
+                <meta name="description" content={description} />
+                <meta name="keywords" content={keyword} />
             </Head>
             <Header />
             {children}
